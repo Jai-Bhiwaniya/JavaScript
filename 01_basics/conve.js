fa499e3cf0107ddc2aae5998f1997c838652f354
Jai Bhiwaniya <jai.bhiwaniya.eee23@itbhu.ac.in>
2026-06-29 20:46:39 +0000

    change all

---
+++ 01_basics/conve.js
@@ -0,0 +1,30 @@
+let score = null
+
+console.log(typeof score) // string
+console.log(typeof (score)) 
+
+let valinnum = Number(score)
+
+console.log(typeof valinnum) //number
+console.log(valinnum) // NaN
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn) // false
+
+// "" => false
+// "Jai" => true
+
+let somenum = 33
+
+let stringNum = String(somenum)
+
+console.log(typeof stringNum) // string
+console.log(stringNum) // "33"
+
