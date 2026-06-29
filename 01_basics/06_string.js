b3db6763aba556fafe7deedb2595f08c9f5f29cc
Jai Bhiwaniya <jai.bhiwaniya.eee23@itbhu.ac.in>
2026-06-29 21:51:53 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,35 @@
+const name = "Jai"
+const repocnt = 50
+
+// console.log(name + repoCnt + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocnt}`);
+
+const gameName = new String('Valorant-04');
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a')); // 1
+
+// const newstr = gameName.substring(-2, 8) // doesnot obey negative index
+// console.log(newstr) // Valorant
+
+// const newstrr = gameName.slice(0,4);
+// console.log(newstrr) // Valor
+
+const str = "  Goku  "
+console.log(str)
+console.log(str.trim()) // Goku
+
+const url = "https://tpc.itbhu.ac.in"
+
+console.log(url.replace('//','//www.')) // https://www.tpc.itbhu.ac.in
+
+console.log(url.includes('ocs')) // false
+
+console.log(url.split('.')) // [ 'https://tpc', 'itbhu', 'ac', 'in' ]
+
+
+
