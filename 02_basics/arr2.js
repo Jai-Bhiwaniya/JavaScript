13005363b823adff5b684fea506e096e3a037afd
Jai Bhiwaniya <jai.bhiwaniya.eee23@itbhu.ac.in>
2026-07-04 21:14:18 +0000

    arrays

---
+++ 02_basics/arr2.js
@@ -0,0 +1,31 @@
+const Marvel = ["thor","Ironman"]
+const DC = ["Flash","Batman"]
+
+// Marvel.push(DC)
+
+// console.log(Marvel)
+// console.log(Marvel[2][1])
+
+// const allheros = Marvel.concat(DC)
+
+// const all_heros = [...Marvel,...DC] //spread
+// console.log(all_heros)
+
+// const another_arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_arr = another_arr.flat(Infinity) // 1 or 2
+
+// console.log(real_arr)
+
+console.log(Array.isArray("Hites"))
+
+console.log(Array.from("Hitesh"))
+
+console.log(Array.from({name: "Hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 400
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
